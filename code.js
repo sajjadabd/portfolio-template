c329061c6cfa41b342e70339eed38bfd13c92215
Sajjad Abdollahi <mynameissajjad@gmail.com>
2020-07-16 20:22:52 +0430

    portfolio template v1.2

--- code.js
+++ code.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
-    //console.log('hello');
-    //$("#photo").css("height" , $(this).width() + '%' );
+        
+
+    // ----------------------------------------------------
+    // Typed.Js 
+
     var options = {
-        strings: ['','^500Software ^500 Engineer', '^500Web ^500 Developer'],
+        strings: ['','^500 Software ^500 Engineer', '^500 Web ^500 Developer' , '^500 Game ^500 Maker' , '^500 Computer ^500 Teacher'],
         typeSpeed: 150,
         backSpeed: 50,
         backDelay: 50,
@@ -13,5 +16,29 @@ $(document).ready(function(){
     };
       
     var typed = new Typed('#typed', options);
+
+    // ---------------------------------------
+    // Scroll Spy 
+
+    inView.offset(200);
+
+    inView('div.part')
+    .on('enter', (el) => {
+        //console.log(el.id);
+        //$(`ul li a`).removeClass('active');
+        $(`a[href='#${el.id}']`).addClass('active');
+    })
+    .on('exit', (el) => {
+        $(`a[href='#${el.id}']`).removeClass('active');
+    });
+
+    // -----------------------------------------
+
+
+    $("div.percent").each( function () {
+        $(this).css("width", $(this).attr('per') + '%' );
+    });
+    
+      
 
 });
