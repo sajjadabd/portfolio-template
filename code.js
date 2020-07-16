d2605c942da98af1105700c3c4b4c7b001ca6d5c
Sajjad Abdollahi <mynameissajjad@gmail.com>
2020-07-17 00:03:12 +0430

    portfolio template v1.3

--- code.js
+++ code.js
@@ -33,11 +33,155 @@ $(document).ready(function(){
     });
 
     // -----------------------------------------
+	// Add Skills
+	
+	frontend = [
+	{ name : "HTML" , per : 90 },
+	{ name : "CSS" , per : 80 },
+	{ name : "Js" , per : 85 },
+	{ name : "Sass" , per : 70 },
+	{ name : "React.Js" , per : 80 },
+	{ name : "Angular" , per : 60 },
+	];
 
+	let listOfSkills = ``;
+		
+	frontend.map( (value) => {
+		listOfSkills += `
+		<div class="skill">
+		<div class="skillname">${value.name}</div> 
+		<div class="barContainer">
+			<div class="bar">
+				<div class="percent" per="${value.per}"></div> 
+			</div>
+			<div class="percentContainer">%${value.per}</div>			
+		</div>
+	</div>
+		`;
+	});
+	
+	$("#frontend").append(listOfSkills);
+	
+	
+	backend = [
+	{ name : "PHP" , per : 80 },
+	{ name : "Laravel" , per : 80 },
+	{ name : "Node.Js" , per : 70 },
+	{ name : "Django" , per : 60 },
+	{ name : "SQL" , per : 90 },
+	{ name : "Sqlite" , per : 90 },
+	{ name : "MongoDb" , per : 90 },
+	];
+	
+	
+	listOfSkills = ``;
+	
+	backend.map( (value) => {
+		listOfSkills += `
+		<div class="skill">
+		<div class="skillname">${value.name}</div> 
+		<div class="barContainer">
+			<div class="bar">
+				<div class="percent" per="${value.per}"></div> 
+			</div>
+			<div class="percentContainer">%${value.per}</div>			
+		</div>
+	</div>
+		`;
+	});
+	
+	$("#backend").append(listOfSkills);
+	
+	
+	
+	
+	
+	
+	programming = [
+	{ name : "C" , per : 70 },
+	{ name : "C++" , per : 80 },
+	{ name : "C#" , per : 80 },
+	{ name : "Java" , per : 90 },
+	{ name : "Python" , per : 90 },
+	{ name : "Dart" , per : 90 },
+	];
+	
+	
+	listOfSkills = ``;
+	
+	programming.map( (value) => {
+		listOfSkills += `
+		<div class="skill">
+		<div class="skillname">${value.name}</div> 
+		<div class="barContainer">
+			<div class="bar">
+				<div class="percent" per="${value.per}"></div> 
+			</div>
+			<div class="percentContainer">%${value.per}</div>			
+		</div>
+	</div>
+		`;
+	});
+	
+	$("#programming").append(listOfSkills);
+	
+	
+	
+	
+	
+	
+	
+	mobile = [
+	{ name : "React Native" , per : 70 },
+	{ name : "Ionics" , per : 80 },
+	{ name : "Flutter" , per : 80 },
+	];
+	
+	
+	listOfSkills = ``;
+	
+	mobile.map( (value) => {
+		listOfSkills += `
+		<div class="skill">
+		<div class="skillname">${value.name}</div> 
+		<div class="barContainer">
+			<div class="bar">
+				<div class="percent" per="${value.per}"></div> 
+			</div>
+			<div class="percentContainer">%${value.per}</div>			
+		</div>
+	</div>
+		`;
+	});
+	
+	$("#mobile").append(listOfSkills);
+	
+	
+	
+	
+	
+	
 
     $("div.percent").each( function () {
         $(this).css("width", $(this).attr('per') + '%' );
     });
+	
+	
+	
+	//------------------------------------------------------------
+	
+	// let body = document.body,
+	// 	html = document.documentElement;
+
+	// let height = Math.max( body.scrollHeight, body.offsetHeight, 
+	// 					html.clientHeight, html.scrollHeight, html.offsetHeight );
+	
+	// console.log(height);
+
+	// $('aside').css('height', height + 'px');
+
+
+	//console.log(document.body.clientWidth);
     
       
 
